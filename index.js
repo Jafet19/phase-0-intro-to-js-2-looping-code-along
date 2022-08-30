7232029b7960454c6c2954881f097d9cbb48e9cd
Jafet19 <kacur99@gmail.com>
2022-08-30 10:09:42 -0500

    commit tree 19d167ee0f239dfe329cf81e262eab49fa641fee

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+function writeCards(){
+    const messages = [ "Thank you, Guadalupe, for the wonderful surprise gift!",
+    "Thank you, Ollie, for the wonderful surprise gift!", 
+    "Thank you, Aki, for the wonderful surprise gift!"]
+    return messages
+}
+function countDown(){
+    let count = 0;
+    while (count < 11){
+        console.log(count++);
+    }
+}
